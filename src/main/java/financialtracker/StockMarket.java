bdd5f016fc37eaac57ede639677f088b93f553aa
zeleney30 <michaelzeleney@gmail.com>
2021-05-27 15:32:38 -0400

    StockMarket class initial

---
+++ src/main/java/financialtracker/StockMarket.java
@@ -0,0 +1,79 @@
+package financialtracker;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static financialtracker.CoinMarketCapApi.makeAPICall;
+
+public class StockMarket
+{
+    public double getStockPrice(String symbol)
+    {
+        String uri = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/info";
+        List<NameValuePair> paratmers = new ArrayList<NameValuePair>();
+
+        paratmers.add(new BasicNameValuePair("symbol", symbol));
+
+        String priceString = "";
+        double price = -1.0;
+
+        try
+        {
+            String result = makeAPICall(uri, paratmers);
+
+            int target = result.indexOf("price of");
+            int decimal = 0;
+            int start;
+            int end;
+
+            while (decimal < target || result.charAt(decimal) != '.')
+            {
+                decimal++;
+            }
+
+            start = decimal;
+            end = decimal;
+
+            while (result.charAt(start) != ' ')
+            {
+                start--;
+            }
+
+            start++;
+
+            while (result.charAt(end) != ' ')
+            {
+                end++;
+            }
+
+            end--;
+
+            while (start != end)
+            {
+                if (result.charAt(start) == ',')
+                    start++;
+
+                priceString = priceString + result.charAt(start);
+                start++;
+            }
+
+            price = Double.parseDouble(priceString);
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error: cannont access content - " + e.toString());
+        }
+        catch (URISyntaxException e)
+        {
+            System.out.println("Error: Invalid URL " + e.toString());
+        }
+
+        return price;
+    }
+}
+
