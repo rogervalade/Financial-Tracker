629130ba4e15255e1e0683010d99fba7e37600ff
zeleney30 <michaelzeleney@gmail.com>
2021-05-28 16:52:48 -0400

    Moved code to Main method to run GUI

--- src/main/java/financialtracker/FinancialTracker.java
+++ src/main/java/financialtracker/FinancialTracker.java
@@ -6,15 +6,10 @@ public class FinancialTracker
 {
     public static void main(String[] args) throws FileNotFoundException
     {
-        StockMarket sm = new StockMarket();
-        Data data = new Data();
-
-        //sm.buyStock("BTC", 1);
-        //sm.buyStock("BTC", 9);
-        //sm.buyStock("BTC", 2);
-
-        //System.out.println(sm.sellStock("BTC", 2));
-
-        //data.removeStock(2.0);
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FintUI().setVisible(true);
+            }
+        });
     }
 }
